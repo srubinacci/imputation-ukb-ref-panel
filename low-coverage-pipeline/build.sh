3014e04a300d2e58012c0c0e80234f31aebd2a89
srubinacci <srubinac@broadinstitute.org>
2023-09-01 16:59:30 -0400

    pipelines

---
+++ low-coverage-pipeline/build.sh
@@ -0,0 +1,4 @@
+dx build tools/glimpse2_phase -d apps/ -f 
+dx build tools/ligate -d apps/ -f 
+dx build tools/split_reference -d apps/ -f 
+dx build low-coverage-pipeline -d apps/ -f 
---
+++ snp-array-pipeline/build.sh
@@ -0,0 +1,5 @@
+dx build tools/impute5 -d apps/ -f 
+dx build tools/shapeit5_ref -d apps/ -f 
+dx build tools/xcftools -d apps/ -f 
+dx build tools/ligate -d apps/ -f 
+dx build snp-array-pipeline -d apps/ -f 
